1dd8b02e1a37c2f957beb9b55b485b97cb8138a6
Jake Bolam <jake.bolam@gmail.com>
2018-04-29 11:41:38 -0400

    Build out results page (#5)
    * wip
    
    * added styling to details page
    
    * put logo on github CDN
    
    * Tweak result page styling

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
+import path from 'path'
 import express from 'express'
 import bodyParser from 'body-parser'
 import serverless from 'serverless-http'
+import mustacheExpress from 'mustache-express'
+import jsonpack from 'jsonpack/main'
+import bytes from 'bytes'
 
 import Store from '../models/store'
 import generateAccessToken from '../helpers/github/generateAccessToken'
@@ -8,10 +12,39 @@ import generateAccessToken from '../helpers/github/generateAccessToken'
 import asyncMiddleware from './middleware/asyncMiddleware'
 import protectedMiddleware from './middleware/protectedMiddleware'
 
+const STATUS = {
+    PASS: 'pass',
+    WARN: 'warn',
+    FAIL: 'fail',
+    REMOVED: 'removed',
+}
+
+const getMustachePropsFromStatus = status => {
+    if (status === STATUS.PASS) {
+        return {
+            pass: true,
+        }
+    } else if (status === STATUS.WARN) {
+        return {
+            warn: true,
+        }
+    } else if (status === STATUS.FAIL) {
+        return {
+            fail: true,
+        }
+    }
+    return {
+        removed: true,
+    }
+}
+
 function createServerlessApp() {
     const app = express()
     app.disable('x-powered-by')
-    // app.use(express.static('public'))
+    app.engine('mustache', mustacheExpress())
+    app.set('view engine', 'mustache')
+    app.set('views', path.join(process.cwd(), 'src/views'))
+    app.use('/static', express.static(path.join(process.cwd(), 'src/static')))
     app.use(bodyParser.json({ strict: false }))
     app.get('/', (req, res) => {
         res.json({ message: 'hello world' })
@@ -76,6 +109,59 @@ function createServerlessApp() {
             res.json({
                 token: result,
             })
+        }),
+    )
+    app.get(
+        '/results',
+        asyncMiddleware(async (req, res) => {
+            let { d } = req.query
+            const unpacked = jsonpack.unpack(d)
+            const results = Object.assign(
+                {},
+                unpacked.results,
+                getMustachePropsFromStatus(unpacked.results.status),
+            )
+            const details = unpacked.details
+
+            details.commitShaPretty = details.commitSha.slice(0, 8)
+            results.fullResults.map(fileResult => {
+                const newFileResult = Object.assign(
+                    fileResult,
+                    getMustachePropsFromStatus(fileResult.status),
+                )
+                newFileResult.prettySize = bytes(fileResult.size)
+                newFileResult.prettyMaxSize = bytes(fileResult.maxSize)
+                if (fileResult.baseBranchSize) {
+                    newFileResult.diff =
+                        fileResult.size - fileResult.baseBranchSize
+                    newFileResult.prettyDiffSize = bytes(newFileResult.diff)
+                    if (newFileResult.diff > 0) {
+                        newFileResult.prettyDiffSize = `+${
+                            newFileResult.prettyDiffSize
+                        }`
+                    }
+                }
+
+                newFileResult.barTotalLength = Math.max(
+                    fileResult.size,
+                    fileResult.maxSize,
+                )
+                newFileResult.baseBranchSizePercentage =
+                    fileResult.baseBranchSize /
+                    newFileResult.barTotalLength *
+                    100
+                newFileResult.diffPercentage =
+                    Math.abs(newFileResult.diff) /
+                    newFileResult.barTotalLength *
+                    100
+                newFileResult.remainingPercentage =
+                    100 -
+                    newFileResult.baseBranchSizePercentage -
+                    newFileResult.diffPercentage
+                return newFileResult
+            })
+            results.status = results.status.toUpperCase()
+            res.render('results', { results, details })
         }),
     )
 
