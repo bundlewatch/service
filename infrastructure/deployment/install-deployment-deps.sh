ca4b815ae9658d1eb736c1f1c8a14734c1c93421
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2022-01-22 10:28:31 -0500

    fix: remove awscli from install-deployment-deps.sh

--- infrastructure/deployment/install-deployment-deps.sh
+++ infrastructure/deployment/install-deployment-deps.sh
@@ -4,7 +4,3 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/../shell-helpers.sh"
 
 run "sudo npm install -g serverless"
-run "sudo apt-get update"
-run "sudo apt-get install python-setuptools python-dev build-essential"
-run "sudo easy_install pip"
-run "sudo pip install awscli"
